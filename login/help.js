9ee054cfd2348fc9d878b78905b2fcbb5d95298e
Badhon <badhonmolla18@gmail.com>
2023-11-09 12:32:06 +0600

    apply

---
+++ login/help.js
@@ -0,0 +1,34 @@
+var main = document.querySelector("#name");
+
+function gmail(){
+    main.innerHTML="***Enter gmail here.";
+}
+function gmail_1(){
+    main.innerHTML="";
+}
+
+function about(){
+    main.innerHTML="***Enter your name here.";
+}
+function about_1(){
+    main.innerHTML="";
+}
+
+function number(){
+    main.innerHTML="***Enter phone number here.";
+}
+function number_1(){
+    main.innerHTML="";
+}
+
+
+
+
+
+
+
+
+
+
+
+
